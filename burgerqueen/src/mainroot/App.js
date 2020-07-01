a1a478920033002a6cee0bede9ffde78efa945b5
estefania moyano <estefaniamoyano@Airdeestefania.home>
2020-07-01 13:28:43 -0400

    rutas funcionado

--- burgerqueen/src/mainroot/App.js
+++ burgerqueen/src/mainroot/App.js
@@ -2,28 +2,30 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
-} from "react-router-dom"; 
+  Route} from "react-router-dom"; 
 import './App.css';
 import Welcome from '../components/Welcome';
 import Buttons from '../components/Buttons';
 
 
-const App = () =>{
-  return ( 
+const App = () => {
+  return (
     <Router>
-     <Switch></Switch>
-       <Route path= "/" exact>
-          <Welcome />
-      <Buttons />
-          </Route>
-        
+      <Switch>
+        <Route path="/" exact>
+          INICIO
+          <Welcome/>
+          <Buttons/>
+
+        </Route>
+        <Route path="/Mesas" exact>
+          MESAS
+        </Route>
+
       </Switch>
+
     </Router>
-     
-
-
   );
+}
 
 export default App;
