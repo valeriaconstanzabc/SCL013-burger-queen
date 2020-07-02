5d029db78f7098305fac2f941dc7c8bedf17d29b
Constanza-Oreste <constanzaorestesalazar@gmail.com>
2020-07-02 12:37:53 -0400

    CSS segunda vista

--- burgerqueen/src/components/TableButton.jsx
+++ burgerqueen/src/components/TableButton.jsx
@@ -4,31 +4,35 @@ import mesa from '../img/mesa.png'
 const TableButton = () => {
     return (
         <main id="tableContainer">
+            <div id="clientTitle">
             <h1>Seleccione mesa de cliente:</h1>
+            </div>
+            <div id="tableButtons">
             <button type="button" className="btnTable">
-                <label className="labelTable">Mesa 1</label>
+                <p className="labelTable">Mesa 1</p>
                 <img src={mesa} alt="mesa" className="imgTable"></img>
             </button>
             <button type="button" className="btnTable">
-                <label className="labelTable">Mesa 2</label>
+                <p className="labelTable">Mesa 2</p>
                 <img src={mesa} alt="mesa" className="imgTable"></img>
                 </button>
             <button type="button" className="btnTable">
-                <label className="labelTable">Mesa 3</label>
+                <p className="labelTable">Mesa 3</p>
                 <img src={mesa} alt="mesa" className="imgTable"></img>
                 </button>
             <button type="button" className="btnTable">
-                <label className="labelTable">Mesa 4</label>
+                <p className="labelTable">Mesa 4</p>
                 <img src={mesa} alt="mesa" className="imgTable"></img>
             </button>
             <button type="button" className="btnTable">
-                <label className="labelTable">Mesa 5</label>
+                <p className="labelTable">Mesa 5</p>
                 <img src={mesa} alt="mesa" className="imgTable"></img>
             </button>
             <button type="button" className="btnTable">
-                <label className="labelTable">Mesa 6</label>
+                <p className="labelTable">Mesa 6</p>
                 <img src={mesa} alt="mesa" className="imgTable"></img>
             </button>
+            </div>
         </main>
     )
 }
