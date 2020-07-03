040cd70766b0c8c9e8ea9426678552e7b6942c68
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-07-03 16:08:56 -0400

    intento de guardar select + css mesas

--- burgerqueen/src/components/InputClient.jsx
+++ burgerqueen/src/components/InputClient.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 const InputClient = () => {
+
+    const [nameClient, setNameClient ] = React.useState('')
+    
     return (
         <div id="client">
             <label id="nameClient">Nombre de cliente:</label>
-            <input id="inputClient" type="text"></input>
+            <input id="inputClient" type="text" onChange = { (e) => setNameClient(e.target.value)}></input>
         </div>
     )
 }
--- burgerqueen/src/components/Select.jsx
+++ burgerqueen/src/components/Select.jsx
@@ -1,13 +1,16 @@
 import React, {Fragment} from 'react'
 
 const Select = () => {
+
+    const [name, setName ] = React.useState('')
+
     return (
         <Fragment>
-            <select id="selectName">
+            <select id="selectName" onChange = { (e) => setName(e.target.value)}>
                 <option value="">Selecciona tu Nombre</option>
-                <option value="">Marta Sanchez</option>
-                <option value="">Juan Carlos Bodoque</option>
-                <option value="">Felipe Pérez</option>
+                <option to="/mesero" value="Marta Sánchez">Marta Sánchez</option>
+                <option value="Juan Carlos Bodoque">Juan Carlos Bodoque</option>
+                <option value="Felipe Pérez">Felipe Pérez</option>
             </select>
         </Fragment>
     )
