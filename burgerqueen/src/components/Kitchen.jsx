5fba6dcde65b843af05cf88a9e51d428e4894563
valeriaconstanzabc <valeria.brito.c@gmail.com>
2020-07-16 16:10:52 -0400

    info boletas en visa cocinas

--- burgerqueen/src/components/Kitchen.jsx
+++ burgerqueen/src/components/Kitchen.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Link } from "react-router-dom";
 import {UserContext} from '../context/UserContext'
 import { firebase } from '../firebase/firebase'
+import '../components/componentsCss/Kitchen.css'
 
 
 
@@ -9,14 +10,16 @@ import { firebase } from '../firebase/firebase'
 const Kitchen = () => {
     
     const [newarray, setNewArray] = useState([])
+    const [idOrderDeliver, setIdOrderDeliver] = useState('')
+
     const getUpate = () => {
         const db = firebase.firestore()
         db.collection('mesas').orderBy('fecha', 'desc').onSnapshot((querySnapshot) =>{
             const docs = []
             querySnapshot.forEach((doc) => {
+            docs.push({...doc.data(), id:doc.id})
             console.log(doc.data())
             console.log(doc.id)
-            docs.push({...doc.data(), id:doc.id})
         })
         console.log(docs)
         setNewArray(docs)
@@ -27,49 +30,63 @@ const Kitchen = () => {
     getUpate()
  }, [])
 
+ const activateOrderDeliver = (item) => {
+    setIdOrderDeliver(item.id)
+    console.log(idOrderDeliver)
+ }
+
+ const addOrderDeliver = () => {
+    const db = firebase.firestore()
+    db.collection('Entregas').doc(idOrderDeliver).update({
+      order: newarray,
+    })
+ }
+
     return (
         <main className="kitcherContainer">
-            <h1></h1>
-            <section className="orderKitchen">
-                <header className="orderTitle">
-                    <div className="containerTittleOrden">
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                        <br/>
-                    <h1>HOLAAAA</h1>
-
-                    </div>
-
-                    <div className="containerClientDateAndHour">
-                        <p className="dateAndHour">Fecha: </p>
-                        {
-                            newarray.map(item => (
-                            <div key={item.id}><p>{item.fecha}</p>
-                            <p>{item.name}</p>
-                            <p>{item.order}</p>
+            {
+                newarray.map((item, index) => (
+                    <section className="orderKitchen">
+                        <div className="orderTitle">
+                            <div className="containerTittleOrden">
+                                <div key={index}>
+                                    <p>{item.name}</p>
+                                </div>        
                             </div>
-                            ))
-                        }
-                        <p className="dateAndHour">Cliente: </p>
-                    </div>
-                </header>
-                <div className="scrollProduct">
-                    <ul className="containerOrderProduct">
-                       
-                        <h3></h3>
-                    </ul>
-               </div>
 
+                            <div className="containerClientDateAndHour">
+                                <div key={index}>
+                                    <p className="dateAndHour">Fecha y hora: {item.fecha}</p>
+                                    <p className="dateAndHour">Mesero: {item.nameWaiter}</p>
+                                    <p className="dateAndHour">Client: {item.nameClient}</p>
+                                </div>
+                            </div>
+                        </div>
 
-            </section>
-            <section className="kitchenButton">
-                <button className="kitchenReady">
-                    Listo
-                </button>
-            </section>
+                        <div className="scrollProduct">
+                            <div className="containerOrderProduct">
+                                <ul key={index}>
+                                    {
+                                    item.order.map(element => 
+                                        <p>{element}</p>
+                                    )}
+                                    
+                                </ul>
+                            </div>
+                        </div>
+
+                        <section className="kitchenButton">
+                            <button key={item.id} className="kitchenReady" onClick = {() => activateOrderDeliver()}>
+                                Listo
+                            </button>
+                            <br/>
+                            <button key={item.id} className="kitchenReady" onClick = {() => addOrderDeliver()}>
+                                Firebase
+                            </button>
+                        </section>
+                    </section>
+                ))
+            }
         </main>
     )
 }
---
+++ burgerqueen/src/components/OrdersToDeliver.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const OrdersToDeliver = () => {
+    return (
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default OrdersToDeliver
